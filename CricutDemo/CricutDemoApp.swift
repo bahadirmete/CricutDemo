4d33437f01ab975390334d3734f3abb36c1a4a3e
Bayram Mete <bayram.mete@aerlingus.com>
2025-03-26 20:16:52 -0500

    Initial Commit

---
+++ CricutDemo/CricutDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CricutDemoApp.swift
+//  CricutDemo
+//
+//  Created by BB Mete on 3/26/25.
+//
+
+import SwiftUI
+
+@main
+struct CricutDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CricutDemoUITests/CricutDemoUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  CricutDemoUITestsLaunchTests.swift
+//  CricutDemoUITests
+//
+//  Created by BB Mete on 3/26/25.
+//
+
+import XCTest
+
+final class CricutDemoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
